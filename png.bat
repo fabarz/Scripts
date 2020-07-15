00e0421bd61fe765e8f1a454607cc400ceffe8a9
fabarz <60856222+fabarz@users.noreply.github.com>
2020-07-15 16:45:20 +0100

    Update png.bat

--- png.bat
+++ png.bat
@@ -1,2 +1,2 @@
 
-C:\Ruby27-x64\bin\ruby -e "trap 'SIGINT' do puts 'Exiting'; exit; end; loop do sleep 0; x = `ping -n 1 tws-palm-qa`; puts Time.new.inspect; puts x; puts end"
+C:\Ruby27-x64\bin\ruby -e "trap 'SIGINT' do puts 'Exiting'; exit; end; loop do sleep 0; x = `ping -n 1 www.google.com`; puts Time.new.inspect; puts x; puts end"
